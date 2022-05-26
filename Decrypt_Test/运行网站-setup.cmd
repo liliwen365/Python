979fbea4073d07b7bdde29f3f9d347cfdb1d9eee
liliwen <76478019+liliwen365@users.noreply.github.com>
2022-05-26 14:34:16 +0800

    新增一键运行网站
    双击“运行网站-setup.cmd”,即可运行网站

---
+++ Decrypt_Test/运行网站-setup.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo *****************************
+echo *Web is start....*
+echo *****************************
+python .\manage.py runserver 127.0.0.1:8000
+
+echo All done successfully!
+pause
+exit
+
